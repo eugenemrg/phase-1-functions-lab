054060981ba626029b015f354d681a1766beb075
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-06-13 20:02:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+const blockLengthInFeet = 264;
+const headquarterBlock = 42;
+
+function distanceFromHqInBlocks(pickupBlock) {
+    return (headquarterBlock > pickupBlock) ? headquarterBlock - pickupBlock : pickupBlock - headquarterBlock;
+}
+
+function distanceFromHqInFeet(pickupBlock){
+    return distanceFromHqInBlocks(pickupBlock) * blockLengthInFeet;
+}
+
+function distanceTravelledInFeet(start, destination){
+    const distanceTravelledInBlocks = (destination > start) ? destination - start : start - destination;
+    return distanceTravelledInBlocks * blockLengthInFeet; 
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    let fare;
+
+    if(distance<=400){
+        fare = 0;
+    }else if(distance>400 && distance<=2000){
+        const chargableDistance = distance - 400;
+        fare = (chargableDistance * 2) / 100;
+    }else if(distance>2000 && distance<=2500){
+        fare = 25;
+    }else if(distance > 2500){
+        return 'cannot travel that far';
+    }
+
+    return fare;
+  }
